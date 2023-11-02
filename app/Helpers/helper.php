318fdb44fa07558357cffa35e91205fa05c80658
wiratama.putra <wiratama.putra@mandalafinance.com>
2023-11-02 13:23:34 +0700

    Initial commit

---
+++ app/Helpers/helper.php
@@ -0,0 +1,22 @@
+<?php
+
+if (! function_exists('status')) {
+    /**
+     * Print status
+     *
+     * Print status dari database dengan nama yang lebih mudah dipahami
+     *
+     * @param string|int $kode_status Kode status dari database
+     * @return String
+     **/
+    function status(string|int $kode_status): string
+    {
+        $status = [
+            0 => 'waiting',
+            1 => 'approve',
+            2 => 'reject',
+        ];
+
+        return $status[$kode_status];
+    }
+}
